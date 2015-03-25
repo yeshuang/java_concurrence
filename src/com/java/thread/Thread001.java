f2623a20ab5cce05199c0e8b312a175458261494
shuang <ye.shuang520@163.com>
2015-03-25 14:12:38 +0800

    测试

---
+++ src/com/java/thread/Thread001.java
@@ -0,0 +1,22 @@
+
+/**
+ * COPYRIGHT (C) 2010 LY. ALL RIGHTS RESERVED.
+ *
+ * No part of this publication may be reproduced, stored in a retrieval system,
+ * or transmitted, on any form or by any means, electronic, mechanical, photocopying,
+ * recording, or otherwise, without the prior written permission of 3KW.
+ *
+ * Created By: Shuang
+ * Created On: 2015年3月25日
+ *
+ * Amendment History:
+ * 
+ * Amended By       Amended On      Amendment Description
+ * ------------     -----------     ---------------------------------------------
+ *
+ **/
+package com.java.thread;
+
+public class Thread001 {
+
+}
